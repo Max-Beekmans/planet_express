c0a58275dd949e3f91cd73d5f8301d3276cf2489
Max Beekmans <mbeekman1@avans.nl>
2017-12-03 15:47:54 +0100

    Filehandler cont

--- FileReader/FileHandler.cpp
+++ FileReader/FileHandler.cpp
@@ -7,29 +7,28 @@
 #include <fstream>
 
 MyString FileHandler::GetLine(const char *file_name) {
-//    std::ifstream file_stream;
-//    char line[64];
-//
-//    file_stream.open(file_name, std::ios::in);
-//
-////    while(file_stream.getline(line, sizeof line, ';')){
-////        std::cout << line;
-////    }
-//
-//    while(isspace((unsigned char)*line)) line++;
-//
-//    char first_char;
-//
-//    file_stream >> first_char;
-//
-//    if((!first_char) && first_char == '#'){
-//        file_stream.ignore(';');
-//    }
-//
-//    while (file_stream.getline(line, sizeof line, ';')) {
-//        line = std::ltrim
+    std::ifstream file_stream;
+    char line[512];
+
+    file_stream.open(file_name, std::ios::in);
+
+//    while(file_stream.getline(line, sizeof line, ';')){
 //        std::cout << line;
 //    }
-//
-//    return MyString {line};
+
+//    while(isspace((unsigned char)*line)) line++;
+
+    char first_char;
+
+    file_stream >> first_char;
+
+    if((!first_char) && first_char == '#'){
+        file_stream.ignore(';');
+    }
+
+    while (file_stream.getline(line, sizeof line, ';')) {
+        std::cout << line;
+    }
+
+    return MyString {line};
 }
